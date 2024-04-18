cb216b1b4123b29b71ec89b07c019d0786c9a26d
bcrhbrhcdb <apet2804@mpsedu.org>
2024-04-18 12:44:24 -0500

    a

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 //get ids
 const quote = document.getElementById('quote')
 const quoteButton = document.getElementById('generateQuote');
+
+const quotes = [
+    {
+        name: 'test',
+        quote: 'textt'
+    }
+]
