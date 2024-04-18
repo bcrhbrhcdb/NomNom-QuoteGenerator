2171fd7967be426ff3118864233dbc38e840799f
bcrhbrhcdb <apet2804@mpsedu.org>
2024-04-18 17:15:32 -0500

    a

--- script.js
+++ script.js
@@ -1,10 +1,44 @@
-//get ids
-const quote = document.getElementById('quote')
-const quoteButton = document.getElementById('generateQuote');
-
+let quote = document.getElementById('quote');
+let name = document.getElementById('name');
+let date = document.getElementById('date');
+let wotd = document.getElementById('wordOfTheDay');
+const quoteButton = document.querySelector('.generateQuote');
 const quotes = [
     {
-        name: 'test',
-        quote: 'text'
+        wordOfTheDay: 'EXCALIBUR',
+        name: 'Alex',
+        quote: 'I am testing this stupid thing',
+        date: '4/18/24'
+    },
+    {
+        wordOfTheDay: '??',
+        name:'Alex',
+        quote:'Texttttt',
+        date:'4/18/24'
     }
 ]
+quoteButton.addEventListener('click', () => {
+    // Get a random quote info from the array
+    const randomQuoteInfo = Math.floor(Math.random() * quotes.length);
+    const randomName = quotes[randomQuoteInfo].name;
+    const randomQuote = quotes[randomQuoteInfo].quote;
+    const randomDate = quotes[randomQuoteInfo].date;
+    const randomWordOfTheDay = quotes[randomQuoteInfo].wordOfTheDay
+// Update the text content of the quote element
+
+if(randomQuote, randomName, randomDate){
+    name.textContent = '';
+    name.textContent += randomName
+    quote.textContent = '';
+    quote.textContent += randomQuote;
+    date.textContent = '';
+    date.textContent += randomDate;
+    wotd.textContent = '';
+    wotd.textContent += randomWordOfTheDay
+}else{
+    name.textContent += randomName
+    quote.textContent += randomQuote;
+    date.textContent += randomDate
+    wotd.textContent += randomWordOfTheDay
+}    
+});
