a91bc437518a530e06bad2fb51f00d5fd683a644
bcrhbrhcdb <apet2804@mpsedu.org>
2024-04-19 11:57:46 -0500

    add some quotes to norm

--- script.js
+++ script.js
@@ -28,15 +28,60 @@ const quotes = [
 ]
 const quotes2 = [
     {
-        name: 'Olieever',
-        quote: 'whats wrong with me, why can i never do anything correct',
-        date: '4/18/24'
+        name: 'Kurt Vonnegut',
+        quote: 'True terror is to wake up one morning and discover that your high school class is running the country',
+        date: 'March 2015'
     },
     {
-        name:'Aleex',
-        quote:'Texttttt',
-        date:'4/18/24'
-    }
+        name:'Oscar Wilde',
+        quote:'Always forgive your enemies; nothing annoys them so much.',
+        date:'June 1954'
+    },
+    {
+        name: 'Jerry Seinfeld',
+        quote:'Sometimes the road less traveled is less traveled for a reason.',
+        date:'February 1997'
+    },
+    {
+        name: 'Phyllis Diller',
+        quote:'Never go to bed mad. Stay up and fight.',
+        date:'November 2016'
+    },
+    {
+        name: 'Judith Martin',
+        quote:'If you can’t be kind, at least be vague.',
+        date:'September 1997'
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
+    {
+        name: '',
+        quote:'',
+        date:''
+    },
 ]
 //add functionallity
 quoteButton.addEventListener('click', () => {
